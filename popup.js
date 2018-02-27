de5b28047c9591bc0c730a629ca1d3cebc107498
Devin Roche <Roche.devin@ymail.com>
2018-02-26 23:02:44 -0800

    partial commit

--- popup.js
+++ popup.js
@@ -1,8 +1,26 @@
-$("body").append('Test');
-$(".dfp-ad-container").each(function() {
-    $(this).replaceWith( "<img width='300' height='250' src='http://thecatapi.com/api/images/get'>" );
-})
-$('#siteTable_organic').hide()
+// $(".dfp-ad-container").each(function() {
+//     $(this).replaceWith( "<img width='300' height='250' src='https://cf.ltkcdn.net/cats/images/std/211655-675x450-Kitten-peeking.jpg'>" );
+// })
+ads = $(".dfp-ad-container")
+
+//.each(function () {
+// $.get('https://dog.ceo/api/breed/poodle/standard/images/random', function (data) {
+//     $(this).replaceWith("<img width='300' height='250' src='" + data.message + "'>");
+// })
+// })
+
+var i=0;
+
+while(i < ads.length){
+    $.get('https://dog.ceo/api/breed/poodle/standard/images/random', function (data) {
+        var ad_id = '#' + ads[i].id
+        console.log(ads)
+        $(ad_id).replaceWith("<img width='300' height='250' src='" + data.message + "'>");
+    })
+    i++;
+}
+
+$('#siteTable_organic, .listingsignupbar').hide()
 
 
 //JJuu2MKJUwauNtQH3mOtGZGDlqGZqmSN
